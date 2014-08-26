954ca9faa4f77cc24dbca92e0387496e735975fd
liuxiangyu <liuxiangyu@live.com>
2014-08-27 01:35:29 +0800

    circuit

--- server/static/javascript/circuit.js
+++ server/static/javascript/circuit.js
@@ -1,41 +1,60 @@
 // Add circuit
 var circuitNum = 0;
+var circuitCounter = 0;
+var circuitFlag = new Array(false, false, false);
 
-var circuits = $("#circuits");
-var cTitle = $("#cTitle");
-var lastCircuit = null;
-var lastTitle = null;
+var circuits = $("#circuits").tabs();
 
 $("#template").hide();
 
-$("#addCircuit").unbind('click');
-
-$("#addCircuit").click(function() {
+$("#addCircuit").unbind('click').click(function() {
     addCircuit();
 });
 
 function addCircuit() {
-    ++circuitNum;
-    var newCircuit = $("#circuit").clone(true);
-    var newli = $("#template").find('.item').clone(true);
-    newCircuit.attr('id', "circuit" + circuitNum);
-    newli.prepend("circuit " + circuitNum);
-    newli.addClass('active');
-    newli.find("a").attr('href', "#circuit" + circuitNum);
-    $("#addCircuit").before(newli);
-    circuits.append(newCircuit);
-    newCircuit.show();
-    newli.show();
-    if (lastCircuit != null) {
-        lastCircuit.hide();
+    if (circuitCounter < 3) {
+        circuitNum = 1;
+        ++circuitCounter;
+        while (circuitFlag[circuitNum - 1])
+            ++circuitNum;
+        circuitFlag[circuitNum - 1] = true;
+        var newCircuit = $("#circuit").clone(true);
+        var newli = $("#template").find('li').clone(true);
+        newCircuit.attr('id', "circuit" + circuitNum);
+        newli.find("a").attr('href', "#circuit" + circuitNum).append("Circuits" + circuitNum);
+        circuits.append(newCircuit);
+        $("#circuits>ul").append(newli);
+        circuits.tabs("refresh");
+        circuits.tabs({active: circuitCounter - 1});
     }
-    if (lastTitle != null) {
-        lastTitle.removeClass("active");
-    }
-    lastTitle = newli;
-    lastCircuit = newCircuit;
+    if (circuitCounter == 3)
+        $("#addCircuit").addClass("disabled");
 }
 
-$("#cTitle>a").click(function() {
-    $(this).
+$("span.ui-icon-close").unbind("click").click(function() {
+    if (circuitCounter == 3) {
+        $("#addCircuit").removeClass("disabled");
+    }
+    var panelId = $( this ).closest( "li" ).remove().attr( "aria-controls" );
+    $( "#" + panelId ).remove();
+    circuitNum = parseInt(panelId.charAt(7));
+    circuitFlag[circuitNum - 1] = false;
+    --circuitCounter;
+    circuits.tabs( "refresh" );
 });
+
+circuits.bind( "keyup", function( event ) {
+    if ( event.altKey && event.keyCode === $.ui.keyCode.BACKSPACE ) {
+        if (circuitCounter == 3) {
+            $("#addCircuit").removeClass("disabled");
+        }
+        var panelId = tabs.find( ".ui-tabs-active" ).remove().attr( "aria-controls" );
+        $( "#" + panelId ).remove();
+        circuitNum = parseInt(panelId.charAt(7));
+        circuitFlag[circuitNum - 1] = false;
+        --circuitCounter;
+        circuits.tabs( "refresh" );
+    }
+});
+
+window.onload = addCircuit;
