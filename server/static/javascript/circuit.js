9894319a338e2c1d64babb4db2826c9b8a3bf263
liuxiangyu <liuxiangyu@live.com>
2014-08-26 17:19:43 +0800

    tab of circuit

--- server/static/javascript/circuit.js
+++ server/static/javascript/circuit.js
@@ -1,7 +1,41 @@
-$(".element").draggable({
-    revert: "invalid"
+// Add circuit
+var circuitNum = 0;
+
+var circuits = $("#circuits");
+var cTitle = $("#cTitle");
+var lastCircuit = null;
+var lastTitle = null;
+
+$("#template").hide();
+
+$("#addCircuit").unbind('click');
+
+$("#addCircuit").click(function() {
+    addCircuit();
 });
 
-$("#canvas-circuit").droppable({
-    accept: ".element-box > div"
+function addCircuit() {
+    ++circuitNum;
+    var newCircuit = $("#circuit").clone(true);
+    var newli = $("#template").find('.item').clone(true);
+    newCircuit.attr('id', "circuit" + circuitNum);
+    newli.prepend("circuit " + circuitNum);
+    newli.addClass('active');
+    newli.find("a").attr('href', "#circuit" + circuitNum);
+    $("#addCircuit").before(newli);
+    circuits.append(newCircuit);
+    newCircuit.show();
+    newli.show();
+    if (lastCircuit != null) {
+        lastCircuit.hide();
+    }
+    if (lastTitle != null) {
+        lastTitle.removeClass("active");
+    }
+    lastTitle = newli;
+    lastCircuit = newCircuit;
+}
+
+$("#cTitle>a").click(function() {
+    $(this).
 });
