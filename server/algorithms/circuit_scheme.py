020f1969b988902c1319e0ef2df8c71cd594b054
Richard Tsai <cairj3@mail2.sysu.edu.cn>
2014-08-31 15:50:21 +0800

    circuit implementations algorithm

--- server/algorithms/circuit_scheme.py
+++ server/algorithms/circuit_scheme.py
@@ -1,13 +1,11 @@
 import uuid
-
-
-def make_schemes(inputs, outputs, truth_table):
-    pass
+import itertools
+from ..models import Gate
 
 
 class _LogicElement(object):
     def __init__(self):
-        self.eid = uuid.uuid4().get_hex()
+        pass
 
     def __and__(self, other):
         if not isinstance(other, _LogicElement):
@@ -22,7 +20,10 @@ class _LogicElement(object):
     def __invert__(self):
         return _LogicGate('NOT', self)
 
-    def implement(self, input_map, _and, _or, _not):
+    def get_all_gates(self):
+        raise NotImplementedError
+
+    def requires(self):
         raise NotImplementedError
 
 
@@ -30,13 +31,18 @@ class _LogicInput(_LogicElement):
     def __init__(self, input_name):
         self.input_name = input_name
 
-    def implement(self, input_map, _and, _or, _not):
-        return []
+    def __repr__(self):
+        return '<%s>' % self.input_name
+
+    def get_all_gates(self):
+        return set()
+
+    def requires(self):
+        return set()
 
 
 class _LogicGate(_LogicElement):
     def __init__(self, logic, input1, input2=None):
-        super(_LogicGate, self).__init__()
         if logic not in ('AND', 'OR', 'NOT'):
             raise ValueError('Invalid logic ' + logic)
         if logic != 'NOT' and input2 is None:
@@ -45,29 +51,143 @@ class _LogicGate(_LogicElement):
         self.input1 = input1
         self.input2 = input2
 
-    def implement(self, input_map, _and, _or, _not):
-        if self.logic == 'AND':
-            gate_desc = _and.to_dict(self.eid)
-        elif self.logic == 'OR':
-            gate_desc = _or.to_dict(self.eid)
-        elif self.logic == 'NOT':
-            gate_desc = _not.to_dict(self.eid)
-        result = [gate_desc]
+    def __eq__(self, other):
+        if not isinstance(other, _LogicGate):
+            return False
+        return self.logic == other.logic and \
+            self.input1 == other.input1 and self.input2 == other.input2
 
-        if isinstance(self.input1, _LogicGate):
-            gate_desc['inputs'] = [self.input1.eid]
-            left_op = self.input1.implement(input_map, _and, _or, _not)
-            left_op[0]['output'] = self.eid
-            result.extend(left_op)
+    def __hash__(self):
+        s = '%d%d%d%d' % (hash(_LogicGate), hash(self.logic),
+                          hash(self.input1), hash(self.input2))
+        return hash(s)
+
+    def __repr__(self):
+        if self.logic == 'NOT':
+            return '(NOT %s)' % repr(self.input1)
         else:
-            gate_desc['inputs'] = [input_map[self.input1.input_name]]
+            return '(%s %s %s)' % (repr(self.input1), self.logic,
+                                   repr(self.input2))
 
+    def get_all_gates(self):
+        gates = {self}
+        if isinstance(self.input1, _LogicGate):
+            gates.update(self.input1.get_all_gates())
         if isinstance(self.input2, _LogicGate):
-            gate_desc['inputs'].append(self.input2.eid)
-            right_op = self.input2.implement(input_map, _and, _or, _not)
-            right_op[0]['output'] = self.eid
-            result.extend(right_op)
-        elif self.input2 is not None:
-            gate_desc['inputs'].append(input_map[self.input2.input_name])
+            gates.update(self.input2.get_all_gates())
+        return gates
 
+    def requires(self):
+        result = {self.logic}
+        if isinstance(self.input1, _LogicGate):
+            result.update(self.input1.requires())
+        if isinstance(self.input2, _LogicGate):
+            result.update(self.input2.requires())
         return result
+
+
+def logic_impelment(logic_exps, input_map, gates):
+    all_gates = set()
+    for l in logic_exps:
+        all_gates.update(l.get_all_gates())
+    all_gates = list(all_gates)
+
+    eid_dict = {}
+    for g in all_gates:
+        if g not in eid_dict:
+            eid_dict[g] = uuid.uuid4().get_hex()
+
+    result = []
+    for g in all_gates:
+        gate_desc = gates[g.logic].to_dict(eid_dict[g])
+
+        if isinstance(g.input1, _LogicGate):
+            gate_desc['inputs'] = [eid_dict[g.input1]]
+        else:
+            gate_desc['inputs'] = [input_map[g.input1]]
+
+        if isinstance(g.input2, _LogicGate):
+            gate_desc['inputs'].append(eid_dict[g.input2])
+        elif g.input2 is not None:
+            gate_desc['inputs'].append(input_map[g.input2])
+
+        result.append(gate_desc)
+
+    outputs = []
+    for l in logic_exps:
+        if isinstance(l, _LogicGate):
+            outputs.append(eid_dict[l])
+        elif isinstance(l, _LogicInput):
+            outputs.append(input_map[l])
+
+    return {'logic_gates': result, 'outputs': outputs}
+
+
+def logic_require(logic_exps):
+    gates = set()
+    for l in logic_exps:
+        gates.update(l.requires())
+    return gates
+
+
+A = _LogicInput('A')
+B = _LogicInput('B')
+_LOGIC_ONE_INPUT = [_LogicElement(), A, ~A, _LogicElement()]
+_LOGIC_TWO_INPUT = [
+    _LogicElement(),
+    A & B,
+    A & ~B,
+    A,
+    ~A & B,
+    B,
+    [(~A & B) | (A & ~B), (~A | ~B) & (A | B)],
+    A | B,
+    ~(A | B),
+    [(A & B) | (~A & ~B), (A | ~B) & (~A | B)],
+    ~B,
+    A | ~B,
+    ~A,
+    ~A | B,
+    ~(A & B),
+    _LogicElement()
+]
+
+
+def validate_truth_table(n_input, n_output, truth_table):
+    pass
+
+
+def make_schemes(inputs, outputs, truth_table):
+    validate_truth_table(len(inputs), len(outputs), truth_table)
+    truth_table.sort(key=lambda x: x['inputs'])
+    logic_exp_lists = []
+    for i, output in enumerate(outputs):
+        idx_orig = [x['outputs'][i] for x in truth_table]
+        idx = 0
+        for k in idx_orig:
+            idx = (idx << 1) | k
+
+        if len(inputs) == 1:
+            logic = _LOGIC_ONE_INPUT[idx]
+            input_map = {A: inputs[0]['eid']}
+        elif len(inputs) == 2:
+            logic = _LOGIC_TWO_INPUT[idx]
+            input_map = {A: inputs[0]['eid'], B: inputs[1]['eid']}
+
+        if isinstance(logic, list):
+            logic_exp_lists.append(logic)
+        else:
+            logic_exp_lists.append([logic])
+
+    gate_impl = {'AND': Gate.query.filter(Gate.logic == 'AND').all(),
+                 'OR': Gate.query.filter(Gate.logic == 'OR').all(),
+                 'NOT': Gate.query.filter(Gate.logic == 'NOT').all()}
+    result = []
+    for logic_exps in itertools.product(*logic_exp_lists):
+        require = list(logic_require(logic_exps))
+        require_impl = [gate_impl[r] for r in require]
+        for an_impl in itertools.product(*require_impl):
+            result.append(logic_impelment(
+                logic_exps, input_map, dict(zip(require, an_impl))))
+
+    return result
