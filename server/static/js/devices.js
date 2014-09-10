ae2e72e860e59cb2877ce55ee96fc42a4eb4cde5
liuxiangyu <liuxiangyu@live.com>
2014-09-11 01:31:45 +0800

    device bug

--- server/static/js/devices.js
+++ server/static/js/devices.js
@@ -139,104 +139,31 @@ g.View = graphiti.Canvas.extend({
             this.setCurrentSelection(null);
         }
 
-        if (figure == null) {
+        /*if (figure == null) {
             g.hideAllToolbar();
-        }
+        }*/
     }
 });
 
 // Creates shapes
 g.Shapes = {};
 
-// shape container
-g.Shapes.Container = graphiti.shape.basic.Rectangle.extend({
-    NAME: "g.Shapes.Container",
-
-    init: function(name) {
-        this._super();
-
-        /*if (typeof radius === "number") {
-          this.setDimension(radius, radius);
-          } else {
-          this.setDimension(1000, 1000);
-          }
-
-          this.TYPE = "Container";
-          this.count = 0;
-          this.countLength = 0;*/
-        //this.selectable = true;
-        //this.draggable = true;
-
-        this.boundElements = new graphiti.util.ArrayList();
-        this.setAlpha(0.01);
-    },
-
-    addItem: function(item) {
-        item.locator = new graphiti.layout.locator.UnbindLocator(this, this.countLength, 100);
-        this.addFigure(item, item.locator); 
-        this.updateContainer();
-    },
-
-    onClick: function(x, y) {
-        var figure = this.getBestFigure(x, y);
-        // console.log(figure);
-        if (figure !== undefined)
-            g.toolbar(figure); 
-    },
-
-    onDoubleClick: function() {
-        g.closeToolbar(this);
-    },
-
-    resetChildren: function() {
-        var that = this;
-        this.children.each(function(i, e) {
-            if (!e.figure.TYPE) {
-                e.figure.setCanvas(null);
-                that.children.remove(e.figure);
-            }
-        });
-        this.repaint();
-    },
-
-    getBestFigure: function(x, y, ignoreType) {
-        var result = null;
-
-        for (var i = 0; i < this.getChildren().getSize(); i++) {
-            var figure = this.getChildren().get(i);
-            if (figure.hitTest(x, y) == true) {
-                if (result === null) {
-                    result = figure;
-                } else if (result.getZOrder() < figure.getZOrder()) {
-                    result = figure;
-                }
-            }
-        };
-
-        if (result !== null)
-            return result;
-    },
-
-    updateContainer: function() { 
-        this.setDimension(this.getWidth(), this.getHeight());
-    }
-});
-
 g.Shapes.Circuit = graphiti.shape.basic.Rectangle.extend({
     NAME: "g.Shapes.Circuit",
 
-    init: function(width, height) {
+    init: function(width, height, name) {
         this._super();
+        if (typeof radius === "number") {
+            this.setDimension(radius, radius);
+        } else {
+            this.setDimension(10, 10);
+        }
         this.boundElements = new graphiti.util.ArrayList();
         this.setAlpha(0.01);
-        this.outputBaseX = 100;
+        this.outputBaseX = 50;
         this.outputBaseY = 0;
         this.inputBaseY = 0;
-        this.name; 
-        this.setDimension(500, 500);
-    },
-
-    setName: function(name) {
+        this.TYPE = "Container";
         this.name = name;
         this.label = new graphiti.shape.basic.Label(name);
         this.label.setColor("#0d0d0d");
@@ -245,53 +172,44 @@ g.Shapes.Circuit = graphiti.shape.basic.Rectangle.extend({
         // add the new decoration to the connection with a position locator.
         //
         this.addFigure(this.label, new graphiti.layout.locator.LeftLocator(this));
-    },
+    }, 
 
     addItem: function(item) {
         if (item.type == "output") {
             item.locator = new graphiti.layout.locator.DeviceLocator(this, this.outputBaseX, this.outputBaseY);
             this.outputBaseY += item.getHeight();
             if (this.outputBaseY > this.height) {
-                this.height = this.inputBaseY;
+                this.height = this.outputBaseY;
             }
-            if (item.getWidth() + this.outputsBaseX > this.width) {
-                this.width = item.getWidth() + this.outputsBaseX;
+            if (item.getWidth() + this.outputBaseX > this.width) {
+                this.width = item.getWidth() + this.outputBaseX;
             }
             this.outputBaseY += 50;
         } else {
             item.locator = new graphiti.layout.locator.DeviceLocator(this, 0, this.inputBaseY);
             this.inputBaseY += item.getHeight();
-            if (item.getWidth() > this.outputBaseX - 100) {
-                this.width += item.getWidth() - this.outputsBaseX + 100;
+            if (item.getWidth() > this.outputBaseX - 50) {
+                this.width += (item.getWidth() - this.outputBaseX + 50);
                 this.outputBaseX = item.getWidth() + 50;
             }
             if (this.inputBaseY > this.height) {
-                this.height = item.getHeight() + this.inputBaseY;
+                this.height = this.inputBaseY;
             }
             this.inputBaseY += 50;
         }
-
         this.addFigure(item, item.locator); 
-        this.updateContainer();
-    },
+        //this.updateContainer();
+        alert(this.width);
+        this.setDimension(this.width, this.height);
+    }, 
 
-    /*onClick: function(x, y) {
-        alert("oh");
+    onClick: function(x, y) {
         var figure = this.getBestFigure(x, y);
-        if (figure !== undefined)
-            g.toolbar(figure); 
-    },*/
-
-    /*resetChildren: function() {
-        var that = this;
-        this.children.each(function(i, e) {
-            if (!e.figure.TYPE) {
-                e.figure.setCanvas(null);
-                that.children.remove(e.figure);
-            }
-        });
-        this.repaint();
-    },*/
+        // console.log(figure);
+        if (figure !== undefined) {
+            figure.onClick(x, y);
+        }
+    },
 
     getBestFigure: function(x, y, ignoreType) {
         var result = null;
@@ -319,33 +237,33 @@ g.Shapes.Circuit = graphiti.shape.basic.Rectangle.extend({
 g.Shapes.Part = graphiti.shape.basic.Rectangle.extend({
     NAME: "g.Shapes.Part",
 
-    init: function(width, height) {
+    init: function(width, height, type) {
         this._super();
+        if (typeof radius === "number") {
+            this.setDimension(radius, radius);
+        } else {
+            this.setDimension(50, 50);
+        }
         this.boundElements = new graphiti.util.ArrayList();
         this.setAlpha(0.01);
-        this.type;
-    },
-
-    setType: function(type) {
+        this.TYPE = "Container";
+        this.draggable = true;
+        this.selectable = true;
         this.type = type;
     },
 
     addItem: function(item, index) {
         item.locator = new graphiti.layout.locator.DeviceLocator(this, index * 2 * item.getWidth(), 0);
-        //if (index > 0) {
-            //var line = new graphiti.shape.basic.Line((index * 2 - 1) * item.getWidth(), 25, index * 2 * item.getWidth() , 25);
-            //line.locator = new graphiti.layout.locator.DeviceLocator(this, (index * 2 - 1) * item.getWidth(), item.getHeight() / 2);
-            //this.addFigure(line, line.locator);
-        //}
-        this.setDimension(this.width + item.getWidth() * 2, item.getHeight());
+        this.setDimension((index * 2 + 1) * item.getWidth(), item.getHeight());
+        //item.locator = new graphiti.layout.locator.ContainerLocator(this, index, 50)
         this.addFigure(item, item.locator); 
-        this.updateContainer();
+        //this.updateContainer();
     }, 
 
     onClick: function(x, y) {
         var figure = this.getBestFigure(x, y);
+        // console.log(figure);
         if (figure !== undefined) {
-            //g.toolbar(figure);
             figure.onClick(x, y);
         }
     },
@@ -365,10 +283,6 @@ g.Shapes.Part = graphiti.shape.basic.Rectangle.extend({
 
         if (result !== null)
             return result;
-    },
-
-    updateContainer: function() { 
-        this.setDimension(this.getWidth(), this.getHeight());
     }
 });
 
@@ -377,16 +291,15 @@ g.Shapes.Biobrick = graphiti.shape.icon.Icon.extend({
     NAME: "g.Shapes.Biobrick",
 
     init: function(width, height, name) {
-        this._super(width, height);
+        this._super();
         this.name = name;
 
-        /*if (typeof radius === "number") {
+        if (typeof radius === "number") {
           this.setDimension(radius, radius);
           } else {
-          this.setDimension(100, 100);
-          }*/
+          this.setDimension(50, 50);
+        }
 
-        this.setDimension(width, height);
         this.setColor("#339BB9");
         //this.TYPE = "Protein";
 
@@ -407,12 +320,12 @@ g.Shapes.Biobrick = graphiti.shape.icon.Icon.extend({
         // add the new decoration to the connection with a position locator.
         //
         this.addFigure(this.label, new graphiti.layout.locator.BottomLocator(this));
+        
     },
 
-    /*onClick: function() {
-        alert("ni");
+    onClick: function() {
         g.toolbar(this);
-    },*/
+    }, 
 
     createSet : function() {
         // var path = this.canvas.paper.path("M0,14.5L6,14.5L6,12L18,12L18,9L24,14.5L30,14.5L30,15.5L24,15.5L18,21L18,18L6,18L6,15.5L0,15.5Z");
@@ -691,11 +604,9 @@ var circuits =
 ];
 
 for (var i = 0; i < circuits.length; ++i) {
-    var circuit = new g.Shapes.Circuit();
-    circuit.setName("Circuit " + (i + 1));
+    var circuit = new g.Shapes.Circuit(0, 0, "Circuit " + (i + 1));
     for (var j = 0; j < circuits[i].inputs.length; ++j) {
-        var input = new g.Shapes.Part();
-        input.setType("input");
+        var input = new g.Shapes.Part(50, 50, "input");
         for (var k = 0; k < circuits[i].inputs[j].length; ++k) {
             var bio = new g.Shapes.Biobrick(50, 50, circuits[i].inputs[j][k].type);
             input.addItem(bio, k);
@@ -704,8 +615,7 @@ for (var i = 0; i < circuits.length; ++i) {
     }
 
     for (var j = 0; j < circuits[i].outputs.length; ++j) {
-        var output = new g.Shapes.Part();
-        output.setType("output");
+        var output = new g.Shapes.Part(50, 50, "output");
         for (var k = 0; k < circuits[i].outputs[j].length; ++k) {
             var bio = new g.Shapes.Biobrick(50, 50, circuits[i].outputs[j][k].type);
             output.addItem(bio, k);
@@ -714,3 +624,13 @@ for (var i = 0; i < circuits.length; ++i) {
     }
     app.view.addFigure(circuit, 100, i * 200);
 }
+/*var test = new g.Shapes.Part();
+for (var i = 0; i < circuits[0].inputs[0].length; ++i) {
+    var bio = new g.Shapes.Biobrick();
+    bio.setName(circuits[0].inputs[0][i].type);
+    test.addItem(bio, i);
+}
+
+app.view.addFigure(test);
+//var bio = new g.Shapes.Biobrick(50, 50, circuits[0].inputs[0][0].type);
+//app.view.addFigure(bio);*/
