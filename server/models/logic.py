bd8f2ef7c7db5f3e10013874e103fc47815d4edd
Richard Tsai <cairj3@mail2.sysu.edu.cn>
2014-09-14 11:39:04 +0800

    update circuit/schemes

--- server/models/logic.py
+++ server/models/logic.py
@@ -1,8 +1,5 @@
 import uuid
 import json
-from .promoter import Promoter
-from .output import Output
-from .RBS import RBS
 from .. import db
 
 
@@ -15,37 +12,22 @@ class Logic(db.Model):
     logic_id = db.Column(db.Integer, primary_key=True)
     logic_name = db.Column(db.String, unique=True)
     n_inputs = db.Column(db.Integer)
-    n_outputs = db.Column(db.Integer)
     truth_table = db.Column(db.String)
-    intermedia = db.Column(db.String)
-    inputs = db.Column(db.String)
-    outputs = db.Column(db.String)
+    intermedia = db.Column(db.String)   # for simulation
+    inputparts = db.Column(db.String)   # for visualization
+    outputparts = db.Column(db.String)  # for visualization
     PRO = db.Column(db.Float)
     RES = db.Column(db.Float)
     SEN = db.Column(db.Float)
     STA = db.Column(db.Float)
     HEA = db.Column(db.Float)
 
-    def _get_inputs_details(self, eid=False):
-        inputs = json.loads(self.inputs)
-        for i in inputs:
-            i['RBS'] = RBS.query.get(i['RBS']).to_dict(eid)
-            i['output'] = Output.query.get(i['output']).to_dict(eid)
-        return inputs
-
-    def _get_outputs_details(self, eid=False):
-        outputs = json.loads(self.outputs)
-        for o in outputs:
-            o['RBS'] = RBS.query.get(o['RBS']).to_dict(eid)
-            o['promoter'] = Promoter.query.get(o['promoter']).to_dict(eid)
-        return outputs
-
     def to_dict(self, eid=False):
         result = {'id': self.logic_id, 'name': self.logic_name,
                   'type': 'logic', 'truth_table': self.truth_table,
                   'intermedia': json.loads(self.intermedia),
-                  'inputs': self._get_inputs_details(eid),
-                  'outputs': self._get_outputs_details(eid),
+                  'inputparts': json.loads(self.inputparts),
+                  'outputparts': json.loads(self.outputparts),
                   'scores': {
                       'PRO': self.PRO,
                       'RES': self.RES,
--- server/views/circuit.py
+++ server/views/circuit.py
@@ -4,44 +4,51 @@ from .. import app
 from ..models import Input, Receptor, Promoter, Output, Logic
 
 
-def _truth_table_satisfies(truth_table, n_outputs, code):
+def _truth_table_satisfies(truth_table, output_idx, code):
     for row in truth_table:
         idx = int(''.join(str(int(x)) for x in row['inputs']), 2)
-        idx *= n_outputs
-        if ''.join('T' if x else 'F' for x in row['outputs']) != \
-                code[idx:idx + n_outputs]:
+        if row['outputs'][output_idx] != (code[idx] == 'T'):
             return False
 
 
-def _get_circuit_schemes(n_inputs, n_outputs, truth_table):
-    logic = []
+def _get_circuit_schemes(inputs, outputs, truth_table):
+    candidates = Logic.query.filter_by(n_inputs=len(inputs)).all()
+    logics = []
 
-    for l in Logic.query.filter_by(n_inputs=n_inputs, n_outputs=n_outputs):
-        if _truth_table_satisfies(truth_table, n_outputs, l.truth_table):
-            logic.append(l.to_dict(True))
+    for i, out in enumerate(outputs):
+        _logic = []
 
-    if n_outputs == 1:
-        if n_inputs == 0:
-            logic.append(None)
-        elif n_inputs == 1 and _truth_table_satisfies(truth_table, 1, 'FT'):
-            logic.append(None)
+        if len(inputs) == 1 and _truth_table_satisfies(truth_table, i, 'TF'):
+            pass  # TODO: Simple circuit
 
-    return logic
+        for l in candidates:
+            if _truth_table_satisfies(truth_table, i, l.truth_table):
+                logic = l.to_dict(True)
+                logic['outputparts'][0].append(out)
+                _logic.append(logic)
+
+        logics.append(_logic)
+
+    return logics
 
 
 @app.route('/circuit/schemes', methods=['POST'])
 def get_circuit_schemes():
     desc = json.loads(request.data)
+
     inputs = []
     for i in desc['inputs']:
-        inputs.append(Input.query.get_or_404(i['id']).to_dict(True))
-        inputs[-1]['receptor'] = Receptor.query.\
-            get_or_404(i['receptor_id']).to_dict(True)
-        inputs[-1]['promoters'] = [Promoter.query.get_or_404(p).to_dict(True)
-                                   for p in i['promoter_ids']]
-    outputs = [Output.query.get_or_404(o).to_dict(True)
-               for o in desc['outputs']]
+        _ = []
+        _.append(Input.query.get_or_404(i['id']))
+        _.append(Receptor.query.get_or_404(i['receptor_id']))
+        for p_id in i['promoter_ids']:
+            _.append(Promoter.query.get_or_404(p_id))
+        inputs.append(_)
 
-    return jsonify(inputs=inputs, outputs=outputs,
-                   logic=_get_circuit_schemes(len(inputs), len(outputs),
-                                              desc['truth_table']))
+    outputs = []
+    for o in desc['outputs']:
+        outputs.append(Output.query.get_or_404(o))
+
+    return jsonify(inputs=inputs,
+                   logics=_get_circuit_schemes(inputs, outputs,
+                                               desc['truth_table']))
