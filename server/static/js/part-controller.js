d280de233023e80d30d2023c97542a71015ee0aa
liuxiangyu <liuxiangyu@live.com>
2014-09-14 15:27:41 +0800

    part

--- server/static/js/part-controller.js
+++ server/static/js/part-controller.js
@@ -474,7 +474,7 @@ var data =
                 "type": "promoter",
                 "name": "promoter XXX",
                 //以下字段仅在Circuit或Device中存在
-                "eid": "brc2abxkdfol66uhjcyi83wn7fz7ftvo",
+                "eid": "c5820968a77f4c278093a048fe60e28a",
             },
             {
                 "id": 1,
@@ -597,7 +597,7 @@ var data =
                 "type": "RBS",
                 "name": "RBS XXX",
                 //以下字段仅在Circuit或Device中存在
-                "eid": "xgzac9q36g8k512saxof8jjly61pmr4v",
+                "eid": "87z53h6uw2zq3pe4aw8tjk6ozj4xiqss",
             },
             {
                 "id": 1,
@@ -661,30 +661,70 @@ var data =
     }],
 
     "relationships": [
-        {
-            "from": "mnr1es2b5ntl2qo9uayu4gye9kw58h68",
-            "to": "9jcjmxdb2t4h8bzde1gmqg5kwhgb4fgu",
-            "type": "promote",
-        },
-        {
-            "from": "kegpghw8wugxtqcpdnqa7dem2719u5rg",
-            "to": "brc2abxkdfol66uhjcyi83wn7fz7ftvo",
-            "type": "repress",
-        }
+    {
+        "from": "mnr1es2b5ntl2qo9uayu4gye9kw58h68",
+        "to": "9jcjmxdb2t4h8bzde1gmqg5kwhgb4fgu",
+        "type": "promote",
+    },
+    {
+        "from": "kegpghw8wugxtqcpdnqa7dem2719u5rg",
+        "to": "xgzac9q36g8k512saxof9jjsy61pmr4v",
+        "type": "repress",
+    }
     ]
 };
-var controller = new g.Application("devices", data);
-var sliderdiv = new g.View("slider");
-var last = 1.0;
-//document.onclick(controller.zoom(0, 0, 1.5));
-Myslider = graphiti.shape.widget.Slider.extend({
-    onValueChange: function(value) {
-        controller.zoom(0, 0, parseFloat(1) / last);
-        last = parseFloat((100 - this.currentValue) / 50) + 0.5;
-        controller.zoom(0, 0, parseFloat((100 - this.currentValue) / 50) + 0.5);
+var promoter = {
+    "id": 1,
+    "type": "promoter",
+    "name": "promoter XXX",
+    //以下字段仅在Circuit或Device中存在
+    "eid": "c5820968a77f4c278093a048fe60e28a"
+};
+
+var rbs = {
+    "id": 1,
+    "type": "RBS",
+    "name": "RBS XXX",
+    //以下字段仅在Circuit或Device中存在
+    "eid": "87z53h6uw2zq3pe4aw8tjk6ozj4xiqss"
+};
+
+var genebio = {
+    "id": 1,
+    "type": "gene",
+    "name": "gene XXX",
+    //以下字段仅在Circuit或Device中存在
+    "eid": "c5820968a77f4c278093a048fe60e28a"
+};
+
+var terminator = {
+    "id": 1,
+    "type": "terminator",
+    "name": "terminator XXX",
+    //以下字段仅在Circuit或Device中存在
+    "eid": "87z53h6uw2zq3pe4aw8tjk6ozj4xiqss"
+};
+var input = new Array();
+var gene = new Array();
+var output = new Array();
+for (var i = 0; i < data.circuits.length; ++i) {
+    for (var j = 0; j < data.circuits[i].inputs.length; ++j) {
+        input.push(promoter);
+        input.push(rbs);
+        input.push(genebio);
+        input.push(terminator);
     }
-});
-var slider = new Myslider();
-slider.setValue(50);
-sliderdiv.addFigure(slider);
-//$(".dimmer").dimmer("hide");
+    for (var j = 0; j < data.circuits[i].logics.length; ++j) {
+        for (var k = 0; k < data.circuits[i].logics[j].inputparts.length; ++k) {
+            for (var m = 0; m < data.circuits[i].logics[j].inputparts[k].length; ++m) {
+                gene.push(data.circuits[i].logics[j].inputparts[k][m]);
+            }
+        }
+        for (var k = 0; k < data.circuits[i].logics[j].outputpart.length; ++k) {
+            output.push(data.circuits[i].logics[j].outputpart[k]);
+        }
+    }
+}
+var inputapp = new g.Application("input", input);
+var geneapp = new g.Application("gene", gene);
+var outputapp = new g.Application("output", output);
