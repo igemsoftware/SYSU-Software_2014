d64662e32924731fd050a5686d042264ee55c197
Richard Tsai <cairj3@mail2.sysu.edu.cn>
2014-09-16 19:09:31 +0800

    model for output-promoter-relationships

--- server/models/output.py
+++ server/models/output.py
@@ -2,6 +2,19 @@ import uuid
 from .. import db
 
 
+class _OutputPromoterRelationship(db.Model):
+    """
+    Models for the relationships between outputs and promoters.
+    """
+    __tablename__ = '_output_promoter_relationships'
+
+    output_id = db.Column(db.Integer, db.ForeignKey('outputs.output_id'),
+                          primary_key=True)
+    promoter_id = db.Column(db.Integer, db.ForeignKey('promoters.promoter_id'),
+                            primary_key=True)
+    relationship = db.Column(db.Enum('promote', 'repress'))
+
+
 class Output(db.Model):
     """
     Model for outputs.
