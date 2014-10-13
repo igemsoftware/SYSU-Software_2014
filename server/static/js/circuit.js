ec33debb41861fda5ddd681a642aef2de6005849
liuxiangyu <liuxiangyu@live.com>
2014-10-13 18:56:29 +0800

    submit valid

--- server/static/js/circuit.js
+++ server/static/js/circuit.js
@@ -19,6 +19,8 @@ var closeicon = $("#template > .delete");
 var step = $("#template .ui.step");
 var repressilator = $("#repressilator");
 var toggletwo = $("#toggletwo");
+var warning = $("#warning");
+var warnmessage = $("#warnmessage");
 var currentcircuit;
 var circuitsArr = [];
 
@@ -34,6 +36,8 @@ function Circuit() {
     this.logicsArr = new Array();
     this.isRepSelected = false;
     this.isTogSwiTwoSelected = false;
+    this.isSingleInput = false;
+    this.isTwoInput = false;
     //this.addPart();
     //this.addOutput();
     this.view.find(".ui.checkbox.mode").checkbox({
@@ -136,6 +140,12 @@ Circuit.prototype.addPart = function(newPart) {
         this.view.find("[name='addTruthTableRow']").removeClass("disabled");
     }
     this.partsArr.push(newPart);
+    if (this.partsArr.length == MAXPARTSNUM || (this.partsArr.length == 1 && this.isSingleInput)) {
+        this.view.find(".parts .items").droppable({disabled:true});
+    }
+    if (this.partsArr.length == 2) {
+        this.isTwoInput = true;
+    }
     var that = this;
     newdelete.click(function() {
         that.deletePart(newPart);
@@ -144,6 +154,14 @@ Circuit.prototype.addPart = function(newPart) {
 
 Circuit.prototype.deletePart = function(delpart) {
     //this.view.find(".parts .items").remove(this.partsArr[index].view);
+    if (this.partsArr.length == 2) {
+        this.isTwoInput = false;
+        for (var i = 0; i < this.logicsArr.length; ++i) {
+            if (this.logicsArr[i] != null && this.logicsArr[i].n_inputs == 2) {
+                this.isTwoInput = true;
+            }
+        }
+    }
     var index = this.partsArr.indexOf(delpart);
     this.partsArr[index].view.remove();
     this.view.find(".truthtable table > thead > tr > th").first().children().last().remove();
@@ -152,6 +170,7 @@ Circuit.prototype.deletePart = function(delpart) {
         row.children("td").first().children().get(index).remove();
         row = row.next();
     }
+    this.view.find(".parts .items").droppable({disabled:false});
     this.partsArr.splice(index, 1);
     this.updateTruthTable();
 }
@@ -333,16 +352,22 @@ function Part(data) {
         cursor: "move",
         start: function(event, ui) {
             if (currentcircuit.partsArr.length < MAXPARTSNUM) {
-                currentcircuit.view.find(".parts .items").droppable({
-                    accept: that.view,
-                    activeClass: "ui-state-highlight",
-                    drop: function( event, ui ) {
-                        inputselector.nextstep();
-                        currentcircuit.addPart(that);
-                    }
-                });
+                if (currentcircuit.partsArr.length == 1 && currentcircuit.isSingleInput) {
+                    warnmessage.html("You have choosed a single input logic gate and a input!");
+                    warning.modal("show");
+                } else {
+                    currentcircuit.view.find(".parts .items").droppable({
+                        accept: that.view,
+                        activeClass: "ui-state-highlight",
+                        drop: function( event, ui ) {
+                            inputselector.nextstep();
+                            currentcircuit.addPart(that);
+                        }
+                    });
+                }
             } else {
-                currentcircuit.view.find(".parts .items").droppable({disabled:true});
+                warnmessage.html("You have choosed input Number is enough!");
+                warning.modal("show");
             }
         }
     });
@@ -766,39 +791,68 @@ function Logic(data) {
         helper: "clone",
         cursor: "move",
         start: function(event, ui) {
-            if (that.data.name.split("-")[0] === "Repressilator") {
+            if (that.data.logic_type === "repressilator") {
                 new Repressilator(that.data);
             } else {
-                currentcircuit.view.find(".logiccontainer").droppable({
-                    accept: that.view,
-                    activeClass: "ui-state-highlight",
-                    drop: function( event, ui ) {
-                        var index = $(this).parent().children().index($(this));
-                        if (that.data.logic_type != "toggle_switch_2") {
-                            var newLogic = new Logic(that.data);
-                            newLogic.littleview.replaceAll($(this));
-                            currentcircuit.outputsArr[index].logicview = newLogic.littleview;
-                            currentcircuit.outputsArr[index].logic = newLogic;
-                            currentcircuit.logicsArr[index] = newLogic;
-                        } else {
-                            new Toggletwo(that.data, index, $(this));
+                if (currentcircuit.isTwoInput && that.data.n_inputs == 1) {
+                    warnmessage.html("This logic is only one input, and you have choosed two input!");
+                    warning.modal("show");
+                } else {
+                    currentcircuit.view.find(".logiccontainer").droppable({
+                        accept: that.view,
+                        activeClass: "ui-state-highlight",
+                        drop: function( event, ui ) {
+                            var index = $(this).parent().children().index($(this));
+                            if (that.data.logic_type != "toggle_switch_2") {
+                                if (that.data.n_inputs == 1) {
+                                    currentcircuit.isSingleInput = true;
+                                } else {
+                                    currentcircuit.isTwoInput = true;
+                                }
+                                var newLogic = new Logic(that.data);
+                                newLogic.littleview.replaceAll($(this));
+                                currentcircuit.outputsArr[index].logicview = newLogic.littleview;
+                                currentcircuit.outputsArr[index].logic = newLogic;
+                                currentcircuit.logicsArr[index] = newLogic;
+                            } else {
+                                new Toggletwo(that.data, index, $(this));
+                            }
                         }
-                    }
-                });
+                    });
+                }
             }
         }
     });
     this.littleview.find(".delete").click(function() {
         var index = $(this).parent().parent().children().index($(this).parent());
+        if (that.data.n_inputs == 1) {
+            currentcircuit.isSingleInput = false;
+        } else {
+            currentcircuit.isTwoInput = false;
+        }
         if (that.data.logic_type != "repressilator") {
             var newview = logiccontainer.clone(true);
             newview.replaceAll($(this).parent());
             currentcircuit.outputsArr[index].logicview = newview;
             currentcircuit.outputsArr[index].logic = null;
+            currentcircuit.logicsArr[index] = null;
         } else {
             $(this).parent().remove();
             currentcircuit.outputsArr.pop();
             currentcircuit.enableDrop();
+            currentcircuit.isRepSelected = false;
+        }
+        for (var i = 0; i < currentcircuit.logicsArr.length; ++i) {
+            if (currentcircuit.logicsArr[i] != null) {
+                if (currentcircuit.logicsArr[i].n_inputs == 1) {
+                    currentcircuit.isSingleInput = true;
+                } else {
+                    currentcircuit.isTwoInput = true;
+                }
+            }
+        }
+        if (currentcircuit.partsArr.length == 2) {
+            currentcircuit.isTwoInput = true;
         }
     });
 }
@@ -941,6 +995,7 @@ function Repressilator(data) {
         currentcircuit.view.find(".logics .items").append(newrepressilator.littleview);
         currentcircuit.logicsArr.push(newrepressilator);
         currentcircuit.disableDrop();
+        currentcircuit.isRepSelected = true;
         that.view.modal("hide");
     });
 }
@@ -999,15 +1054,47 @@ $(document).ready(function() {
 $("#upload").click(function() {
     var circuits = new Array();
     var details = new Array();
+    var message = "You didn't design any circuit";
+    var valid = false;
     for (var i = 0; i < circuitsArr.length; ++i) {
+        valid = true;
+        message = "";
         if (circuitFlag[i]) {
-            circuits.push(circuitsArr[i].uploaddata());
-            details.push(circuitsArr[i].getDetail());
+            if (circuitsArr[i].logicsArr.length == 0) {
+                message += "Circuit " + (i + 1) + " is empty.";
+                valid = false;
+                break;
+            } else if (!circuitsArr[i].isRepSelected) {
+                if (circuitsArr[i].partsArr.length == 0) {
+                    message += "Circuit " + (i + 1) + " has no input";
+                } else if (circuitsArr[i].isTwoInput && circuitsArr[i].partsArr.length == 1) {
+                    message += "Circuit " + (i + 1) + " input number is not";
+                }
+                if (circuitsArr[i].outputsArr.length == 0) {
+                    message += "Circuit " + (i + 1) + " has no output";
+                } else if (circuitsArr[i].isTogSwiTwoSelected && circuitsArr[i].outputsArr.length == 1) {
+                    message += "Circuit " + (i + 1) + " output number is not";
+                }
+                for (var j = 0; j < circuitsArr[i].length; ++j) {
+                    if (circuitsArr[i].logicsArr[j] == null) {
+                        message += "some logic didn't choose";
+                        break;
+                    }
+                }
+                valid = false;
+                break;
+            } else {
+                circuits.push(circuitsArr[i].uploaddata());
+                details.push(circuitsArr[i].getDetail());
+            }
         }
     }
-    if (circuits.length > 0) {
+    if (valid) {
         sessionStorage.setItem("circuits", JSON.stringify(circuits));
         sessionStorage.setItem("preprocess", JSON.stringify(details));
         window.location.href = "/shape";
+    } else {
+        warnmessage.html(message);
+        warning.modal("show");
     }
 });
