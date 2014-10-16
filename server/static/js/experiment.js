370e0a9c167f0070c3d9e9673fa58b7e92c97eb7
JieweiWei <weijieweijerry@163.com>
2014-10-17 04:32:18 +0800

    update simulate

---
+++ server/static/js/experiment.js
@@ -0,0 +1,5 @@
+
+$(function() {
+    $( "#procedure" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+    $( "#procedure li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+});
