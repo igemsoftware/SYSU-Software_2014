040ff52a12fd44c422412548cd34ca101d9b7f9d
liuxiangyu <liuxiangyu@live.com>
2014-10-22 12:00:47 +0800

    final update

--- server/static/js/simulation-data.js
+++ server/static/js/simulation-data.js
@@ -79,7 +79,6 @@ $(function() {
                         },
                     });
 
-
                     /* Get dynamic data. */
                     $.ajax({
                         type: 'POST',
